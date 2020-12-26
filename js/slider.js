00c1d1db4020462fa97872da0ed67823f6724b79
jeki91 <esmet@mail.ua>
2020-12-26 12:51:08 +0300

    v 2.0

--- js/slider.js
+++ js/slider.js
@@ -3,4 +3,14 @@ new Swiper('.online', {
 		nextEl: '.next-offer',
 		prevEl: '.prev-offer',
 	},
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'fraction',
+	},
+	loop: true,
+	effect: 'fade',
+	fadeEffect: {
+		crossFade: true,
+	},
+	autoHeight: true,
 });
